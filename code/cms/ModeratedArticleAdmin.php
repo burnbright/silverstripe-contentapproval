def9c76b43a08b7bd5506760ee4b9151c29f1a31
Jeremy Shipman <jeremy@burnbright.net>
2011-08-25 03:25:10 +0000

    menu priority

--- code/cms/ModeratedArticleAdmin.php
+++ code/cms/ModeratedArticleAdmin.php
@@ -1,13 +1,13 @@
 <?php
 class ModeratedArticleAdmin extends ModelAdmin{
-	
+
 	static $url_segment = 'articlemoderation';
 	static $menu_title = 'Articles';
-	
+	static $menu_priority = 7;
+
 	static $managed_models = array(
 		'ModeratedArticle'
 	);
 
-	
 }
 ?>
